dbe512842f445ad27f941d653a05758cf1c1cfb9
Shamira Naznin <shaminzn@gmail.com>
2020-03-11 23:52:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   obj[key] = value;
   return obj;
 }
